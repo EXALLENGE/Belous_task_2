8fb9b3abb9035ea3f41a21aeb608571ad6eac4bc
PsychoBel <mike.belous@gmail.com>
2019-02-19 22:01:06 +0300

    Game gallow

---
+++ Gallows/game_gallows.py
@@ -0,0 +1,51 @@
+import random
+
+dict = ("apple", "orange", "mandarin", "watermelon", "strawberry")
+text = '''                  Game Gallows
+Rules: 1. You need to guess the word on the topic of fruits
+       2. You can be mistaken only 5 times 
+       3. By '*' denotes words that you haven't guessed
+       4. All letters of the word are small 
+       
+If you want to start the game, press - '1'
+If you want exit the game press - '0'  '''
+print (text)
+if int(input()) == 1:
+    word = dict[random.randint(0, 4)]
+    print ("Word you need to guess:", end = " ")
+    check = []
+    for i in range (0, len(list(word))):
+        check.append("*")
+    print(''.join(check))
+    print("\n")
+    mistake = 0
+    while mistake < 5:
+        mis = False
+        print ("Write a letter: ")
+        letter = input()
+        for i in range (0, len(list(word))):
+            if letter == list(word)[i]:
+                check[i] = letter
+                mis = True
+        if mis:
+            if check == list(word):
+                print("Congratulations! You won!")
+                break
+            else:
+                print("You guessed the letter")
+                print(''.join(check))
+        else:
+            mistake += 1
+            print("This letter isn't in the word")
+            print("Number of mistakes: " + str(mistake) + " of 5")
+            print(''.join(check))
+    if mistake == 5:
+        print("You lost!")
+    else:
+        raise SystemExit
+
+
+else:
+    raise SystemExit
+
+
